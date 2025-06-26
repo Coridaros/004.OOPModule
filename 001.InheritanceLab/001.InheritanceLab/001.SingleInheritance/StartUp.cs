15c98ca1b0aaaca606f168403ec5e50ca23d4cf7
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-26 22:37:37 +0300

    add

---
+++ 001.InheritanceLab/001.InheritanceLab/001.SingleInheritance/StartUp.cs
@@ -0,0 +1,15 @@
+﻿//Inheritance - lab, Task 001
+//24.06.2025, 00:09
+
+namespace Farm;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Dog dog = new Dog();
+
+        dog.Eat();
+        dog.Bark();
+    }
+}
