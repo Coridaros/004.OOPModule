5764cb3e27c6d4f628c64f110df2ffd79efcb8f3
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-26 22:40:52 +0300

    add

---
+++ 001.InheritanceLab/005.StackОfStrings/StackOfStrings.cs
@@ -0,0 +1,24 @@
+﻿namespace CustomStack;
+
+public class StackOfStrings : Stack<string>
+{
+    public bool IsEmpty()
+    {
+        if (Count < 1)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public Stack<string> AddRange(IEnumerable<string> items)
+    {
+        foreach (var item in items)
+        {
+            Push(item);
+        }
+
+        return this;
+    }
+}
---
+++ 001.InheritanceLab/005.StackОfStrings/StartUp.cs
@@ -0,0 +1,12 @@
+﻿//Inheritance - lab, Task 005
+//26.06.2025, 22:32
+
+namespace CustomStack;
+
+public class StartUp
+{
+    static void Main()
+    {
+
+    }
+}
