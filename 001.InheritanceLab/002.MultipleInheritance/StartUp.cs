5ce82c93bdb8305a4c9a0559355a342089a30d54
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-26 22:39:14 +0300

    add

---
+++ 001.InheritanceLab/002.MultipleInheritance/StartUp.cs
@@ -0,0 +1,32 @@
+﻿//Inheritance - lab, Task 002
+//26.06.2025, 22:08
+
+namespace Farm;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Console.WriteLine("Animal....");
+
+        Animal animal = new Animal();
+        animal.Eat();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Dog...");
+
+        Dog dog = new Dog();
+        dog.Eat();
+        dog.Bark();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Puppy...");
+
+        Puppy puppy = new Puppy();
+        puppy.Eat();
+        puppy.Bark();
+        puppy.Weep();
+    }
+}
