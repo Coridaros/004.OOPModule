3512444deffa5c08a764633baa267cfd7a193bac
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-26 22:40:27 +0300

    add

---
+++ 001.InheritanceLab/004.RandomList/RandomList.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomRandomList;
+
+public class RandomList : List<string>
+{
+    public string RandomString(List<string> list)
+    {
+        Random rnd = new Random();
+
+        string result = list[rnd.Next(0, list.Count + 1)];
+
+        return result;
+    }
+}
---
+++ 001.InheritanceLab/004.RandomList/StartUp.cs
@@ -0,0 +1,12 @@
+﻿//Inheritance - lab, Task 004
+//26.06.2025, 22:23
+
+namespace CustomRandomList;
+
+public class StartUp
+{
+    static void Main()
+    {
+
+    }
+}
