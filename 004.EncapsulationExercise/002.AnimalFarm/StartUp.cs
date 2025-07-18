f9d910d65c90f621c22462303ef8fa9ea8f57cb6
NeonCardinal <nvnnaidenov@gmail.com>
2025-07-18 21:42:21 +0300

    add

---
+++ 004.EncapsulationExercise/002.AnimalFarm/StartUp.cs
@@ -0,0 +1,28 @@
+﻿//Encapsulation - exercise, Task 002
+//17.07.2025, 23:47
+using AnimalFarm.Models;
+
+namespace AnimalFarm;
+
+public class StartUp
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+
+        try
+        {
+            Chicken chicken = new Chicken(name, age);
+            Console.WriteLine(
+                "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                chicken.Name,
+                chicken.Age,
+                chicken.ProductPerDay);
+        }
+        catch(ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
