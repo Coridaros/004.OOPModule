4681aebf216380c1f919e6b8b3dd7cb841cb6664
NeonCardinal <nvnnaidenov@gmail.com>
2025-08-26 23:13:26 +0300

    add

---
+++ 005.InterfacesAndAbstractionLab/001.Shapes/StartUp.cs
@@ -0,0 +1,21 @@
+﻿//Interfaces and abstraction - lab, Task 001
+//09.08.2025, 17:72
+
+namespace Shapes;
+
+public class StartUp
+{
+    static void Main()
+    {
+        var radius = int.Parse(Console.ReadLine());
+        IDrawable circle = new Circle(radius);
+
+        var width = int.Parse(Console.ReadLine());
+        var height = int.Parse(Console.ReadLine());
+        IDrawable rect = new Rectangle(width, height);
+
+        circle.Draw();
+        rect.Draw();
+
+    }
+}
