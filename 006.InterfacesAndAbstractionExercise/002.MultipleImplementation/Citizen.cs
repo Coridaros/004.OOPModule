12c1fbd6e6b1f92d0f44d19c267e78eb54d92878
NeonCardinal <nvnnaidenov@gmail.com>
2025-08-27 16:20:39 +0300

    add

---
+++ 006.InterfacesAndAbstractionExercise/002.MultipleImplementation/Citizen.cs
@@ -0,0 +1,20 @@
+﻿namespace PersonInfo;
+
+public class Citizen : IBirthable, IIdentifiable, IPerson
+{
+    public string Birthdate { get; private set; }
+
+    public string Id { get; private set; }
+
+    public string Name { get; private set; }
+
+    public int Age { get; private set; }
+
+    public Citizen(string name, int age, string id, string birthDate)
+    {
+        Name = name;
+        Age = age;
+        Id = id;
+        Birthdate = birthDate;
+    }
+}
