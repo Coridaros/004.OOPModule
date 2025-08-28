eb5b23d5d9a309c8a143863baf74af034c60cb19
NeonCardinal <nvnnaidenov@gmail.com>
2025-08-28 15:29:23 +0300

    add

---
+++ 007.PolymorphismLab/002.Animals/StartUp.cs
@@ -0,0 +1,16 @@
+﻿//Polymorphism - lab, Task 002
+//27.08.2025, 15:39
+
+using Animals;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Animal cat = new Cat("Peter", "Whiskas");
+        Animal dog = new Dog("George", "Meat");
+
+        Console.WriteLine(cat.ExplainSelf());
+        Console.WriteLine(dog.ExplainSelf());
+    }
+}
