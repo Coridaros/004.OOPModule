f4d3c73348f8de5a2afdccbf3bd977773eaf0a38
NeonCardinal <nvnnaidenov@gmail.com>
2025-08-28 15:28:54 +0300

    add

---
+++ 007.PolymorphismLab/001.MathOperations/StartUp.cs
@@ -0,0 +1,16 @@
+﻿//Polymorphism - lab, Task 001
+//27.08.2025, 15:31
+
+namespace Operations;
+
+public class StartUp
+{
+    static void Main()
+    {
+        MathOperations mo = new MathOperations();
+
+        Console.WriteLine(mo.Add(2, 3));
+        Console.WriteLine(mo.Add(2.2, 3.3, 5.5));
+        Console.WriteLine(mo.Add(2.2m, 3.3m, 4.4m));
+    }
+}
