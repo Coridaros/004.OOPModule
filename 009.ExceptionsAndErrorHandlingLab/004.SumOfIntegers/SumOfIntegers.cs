8f9e5cdc08790b8e544ea0bc668bde398a07a138
NeonCardinal <nvnnaidenov@gmail.com>
2025-09-02 23:45:17 +0300

    add

---
+++ 009.ExceptionsAndErrorHandlingLab/004.SumOfIntegers/SumOfIntegers.cs
@@ -0,0 +1,34 @@
+﻿//Exceptions and error handling - lab, Task 004
+//27.08.2025, 18:13
+
+public class SumOfIntegers
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+
+        long sum = 0;
+
+        foreach(var item in input)
+        {
+            try
+            {
+                int number = int.Parse(item);
+
+                sum += number;
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine($"The element '{item}' is in wrong format!");
+            }
+            catch (OverflowException oe)
+            {
+                Console.WriteLine($"The element '{item}' is out of range!");
+            }
+
+            Console.WriteLine($"Element '{item}' processed - current sum: {sum}");
+        }
+
+        Console.WriteLine($"The total sum of all integers is: {sum}");
+    }
+}
