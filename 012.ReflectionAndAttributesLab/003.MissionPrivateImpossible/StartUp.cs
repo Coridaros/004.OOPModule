bf07a860f0c3bb49db220faafa0d118b2e03d18d
NeonCardinal <nvnnaidenov@gmail.com>
2025-09-07 01:31:53 +0300

    add

---
+++ 012.ReflectionAndAttributesLab/003.MissionPrivateImpossible/StartUp.cs
@@ -0,0 +1,18 @@
+﻿//Reflection and attributes - lab, Task 003
+//06.09.2025, 23:52
+
+using System;
+
+namespace Stealer;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Spy spy = new Spy();
+
+        string result = spy.RevealPrivateMethods("Stealer.Hacker");
+
+        Console.WriteLine(result);
+    }
+}
