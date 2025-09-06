5e23daa224e9b7f9ae27cbe5ef88b33611dfcd63
NeonCardinal <nvnnaidenov@gmail.com>
2025-09-07 01:31:27 +0300

    add

---
+++ 012.ReflectionAndAttributesLab/002.HighQualityMistakes/Spy.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer;
+
+public class Spy
+{
+    public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        Type type = Type.GetType(investigatedClass);
+
+        FieldInfo[] fieldInfo = type.GetFields(BindingFlags.Instance | BindingFlags.Public
+            | BindingFlags.NonPublic);
+
+        object inst = Activator.CreateInstance(type);
+
+        sb.AppendLine($"Class under investigation: {type}");
+
+        foreach (FieldInfo field in fieldInfo.Where(f => requestedFields.Contains(f.Name)))
+        {
+            sb.AppendLine($"{field.Name} = {field.GetValue(inst)}");
+        }
+
+        return sb.ToString().Trim();
+    }
+
+    public string AnalyzeAccessModifiers(string className)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        Type type = Type.GetType(className);
+        FieldInfo[] fieldInfos = type.GetFields(BindingFlags.Instance | BindingFlags.Public);
+        MethodInfo[] gettersInfo = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+        MethodInfo[] settersInfo = type.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+
+
+        foreach(FieldInfo field in fieldInfos)
+        {
+            sb.AppendLine($"{field.Name} must be private!");
+        }
+
+        foreach(MethodInfo getter in gettersInfo.Where(g => g.Name.StartsWith("get")))
+        {
+            sb.AppendLine($"{getter.Name} have to be public!");
+        }
+
+        foreach(MethodInfo setter in settersInfo.Where(s => s.Name.StartsWith("set")))
+        {
+            sb.AppendLine($"{setter.Name} have to be private!");
+        }
+
+        return sb.ToString().Trim();
+    }
+}
---
+++ 012.ReflectionAndAttributesLab/002.HighQualityMistakes/StartUp.cs
@@ -0,0 +1,16 @@
+﻿//Reflection and attrubutes - lab, Task 002
+//06.09.2025, 22:46
+
+namespace Stealer;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Spy spy = new Spy();
+
+        string result = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+
+        Console.WriteLine(result);
+    }
+}
