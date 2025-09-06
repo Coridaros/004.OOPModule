60515ce0c5fe04dbb50fa18099c8fd47b64699de
NeonCardinal <nvnnaidenov@gmail.com>
2025-09-07 01:32:17 +0300

    add

---
+++ 012.ReflectionAndAttributesLab/004.Collector/StartUp.cs
@@ -0,0 +1,16 @@
+﻿//Reflection and attributes - lab, Task 004
+//06.09.2025, 00:17
+
+namespace Stealer;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Spy spy = new Spy();
+
+        string result = spy.CollectGettersAndSetters("Stealer.Hacker");
+
+        Console.WriteLine(result);
+    }
+}
