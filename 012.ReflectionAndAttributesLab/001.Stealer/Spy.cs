d7ff057bb6ef35488b29d283bc6e83cc8537dba9
NeonCardinal <nvnnaidenov@gmail.com>
2025-09-07 01:31:01 +0300

    add

---
+++ 012.ReflectionAndAttributesLab/001.Stealer/Spy.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer;
+
+public class Spy
+{
+    public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        Type type = Type.GetType(investigatedClass);
+
+        FieldInfo[] fieldInfo = type.GetFields(BindingFlags.Instance | BindingFlags.Public
+            | BindingFlags.NonPublic);
+
+        object inst = Activator.CreateInstance(type);
+
+        sb.AppendLine($"Class under investigation: {type}");
+
+        foreach(FieldInfo field in fieldInfo.Where(f => requestedFields.Contains(f.Name)))
+        {
+            sb.AppendLine($"{field.Name} = {field.GetValue(inst)}");
+        }
+
+        return sb.ToString().Trim();
+    }
+}
---
+++ 012.ReflectionAndAttributesLab/001.Stealer/StartUp.cs
@@ -0,0 +1,16 @@
+﻿//Reflection and attributes - lab, Task 001
+//06.09.2025, 22:11
+
+namespace Stealer;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Spy spy = new Spy();
+
+        string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+
+        Console.WriteLine(result);
+    }
+}
