2ebf6075f75380693b8f27d5fb21492579cf5491
NeonCardinal <nvnnaidenov@gmail.com>
2025-09-08 13:29:49 +0300

    add

---
+++ 013.ReflectionAndAttributesExercise/001.CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,24 @@
+﻿using CommandPattern.Core.Contracts;
+using System.Reflection;
+
+namespace CommandPattern.Core;
+
+public class CommandInterpreter : ICommandInterpreter
+{
+    public string Read(string args)
+    {
+        string[] arguments = args.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        string commandName = arguments[0];
+        string[] commmandArgs = arguments.Skip(1).ToArray();
+
+        Type commandType = Assembly.GetEntryAssembly().GetTypes()
+            .FirstOrDefault(t => t.Name.Equals($"{commandName}Command"));
+
+        ICommand commandInstance = Activator.CreateInstance(commandType) as ICommand;
+
+        string result = commandInstance.Execute(commmandArgs);
+
+        return result;
+    }
+}
---
+++ 013.ReflectionAndAttributesExercise/001.CommandPattern/StartUp.cs
@@ -0,0 +1,17 @@
+﻿//Reflection and Attributes - exercise, Task 001
+//07.09.2025, 18:40
+
+using CommandPattern.Core;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern;
+
+public class StartUp
+{
+    static void Main()
+    {
+        ICommandInterpreter command = new CommandInterpreter();
+        IEngine engine = new Engine(command);
+        engine.Run();
+    }
+}
